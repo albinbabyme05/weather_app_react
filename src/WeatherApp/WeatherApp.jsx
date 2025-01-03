a1ceca67705f3d8c47e966b11d1c053c146267c4
Albin Baby <albinbabayme05@gmail.com>
2025-01-03 23:43:42 +0100

    search and reset button added and the input field for city also created

--- src/WeatherApp/WeatherApp.jsx
+++ src/WeatherApp/WeatherApp.jsx
@@ -6,12 +6,19 @@ const WeatherApp =()=>{
   const [city, setCity] = useState('');
   const [weatherData, setWeatherData] = useState(null)
   const [error, setError] = useState('');
+ 
 
   const fetchData =(city)=>{
     const API_KEY = "246c2956423a4a9ab0e144605250301";
     axios.get(` http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`)
-    .then(response=>{
-      console.log(response.data)
+    .then((response)=>{
+      setWeatherData(response.data)
+      console.log(response.data);
+      
+      setError('')
+    }).catch((err)=>{
+      setError('City is not Found')
+      setWeatherData(null)
     })
   }
 
@@ -20,21 +27,35 @@ const WeatherApp =()=>{
     fetchData(city)
   }
 
+  const handleReset =()=>{
+    return(
+      setCity('')
+    )
+  }
+
   return(
-      <div className='search-form-container'>
-    <form className='search-form' onSubmit={handleSubmit}>
-      <input
-        type="text"
-      value={city}
-      placeholder='Enter City'
-      onChange={(e)=>{setCity(e.target.value)}}
-      />
+      <div className="weather-app">
 
-      <button type='submit' >Search</button>
+        
+          <div className='search-form-container'>
+            <form className='search-form' onSubmit={handleSubmit}>
+              <input
+                type="text"
+              value={city}
+              placeholder='Enter City'
+              onChange={(e)=>{setCity(e.target.value)}}
+              />
 
-    </form>
-    
-  </div>
+              <button type='submit' >Search</button>
+              <button onClick={handleReset}>Reset</button>
+              
+
+            </form>
+            
+          </div>
+          
+      </div>
+      
   )
 }
 
